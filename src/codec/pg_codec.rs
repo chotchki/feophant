3f45bd530024cef4989c4e56ca226af50a732bf4
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2021-05-09 16:24:34 -0400

    Fixed subtle errors in how I assume message types.

--- src/codec/pg_codec.rs
+++ src/codec/pg_codec.rs
@@ -16,36 +16,37 @@ impl Decoder for PgCodec {
         &mut self,
         src: &mut BytesMut
     ) -> Result<Option<Self::Item>, Self::Error> {
-        if src.len() < 4 {
-            // Not enough data to make a decision.
-            return Ok(None);
-        }
-
-        //Read the first 4 bytes since it could be the special SSL message or a real message.
-        let mut initial_bytes = [0u8; 4];
-        initial_bytes.copy_from_slice(&src[..4]);
-
-        //Handle all special messages
-        if initial_bytes == SSL_PAYLOAD {
-            src.advance(4);
-            return Ok(Some(NetworkFrame::new(0, 4, Bytes::copy_from_slice(&initial_bytes))));
-        }
-
-        //Now we actually have to deal with real messages
         if src.len() < 5 {
             // Not enough data to make a decision.
             return Ok(None);
         }
 
+        //Read the first byte
         let mut message_bytes = [0u8; 1];
         message_bytes.copy_from_slice(&src[..1]);
         let message_type = u8::from_be(message_bytes[0]);
 
+        //If the message_type is 0, then it doesn't have a type and should just be seen as the length
+        let prefix_len;
+        if message_type == 0 {
+            prefix_len = 4;
+        } else {
+            prefix_len = 5;
+        }
         let mut length_bytes = [0u8; 4];
-        length_bytes.copy_from_slice(&src[1..5]);
+        length_bytes.copy_from_slice(&src[(prefix_len-4)..prefix_len]);
 
         let length = u32::from_be_bytes(length_bytes) as u32;
-        let length_size = u32::from_be_bytes(length_bytes) as usize;
+
+        if length < 4 {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                format!("Frame length of {} is too small", length)
+                ));
+        }
+        let length = length - 4;
+        let length_size = u32::from_be_bytes(length_bytes) as usize - 4;
+
 
         // TODO - Unsure how to stop DDOS when the protocol allows up to 2GB of data
         //          Would be great to know if the user is authenticated
@@ -58,12 +59,12 @@ impl Decoder for PgCodec {
         //    ));
         //}
 
-        if src.len() < 5 + length_size {
+        if src.len() < prefix_len + length_size {
             // The full payload has not yet arrived.
             //
             // We reserve more space in the buffer. This is not strictly
             // necessary, but is a good idea performance-wise.
-            src.reserve(5 + length_size - src.len());
+            src.reserve(prefix_len + length_size - src.len());
 
             // We inform the Framed that we need more bytes to form the next
             // frame.
@@ -72,8 +73,8 @@ impl Decoder for PgCodec {
 
         // Use advance to modify src such that it no longer contains
         // this frame.
-        let data = src[5..5 + length_size].to_vec();
-        src.advance(5 + length_size);
+        let data = src[prefix_len..prefix_len + length_size].to_vec();
+        src.advance(prefix_len + length_size);
 
         // Convert the data to a string, or fail if it is not valid utf-8.
         Ok(Some(NetworkFrame::new(message_type, length, Bytes::from(data))))
