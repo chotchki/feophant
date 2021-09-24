830ac8dcf9d844e5c1a15faa1df2e2adedd2c90e
Christopher Hotchkiss <christopher.hotchkiss@gmail.com>
2021-09-24 14:28:28 -0400

    Adding the new trait mod

--- src/engine/io/format_traits.rs
+++ src/engine/io/format_traits.rs
@@ -2,3 +2,9 @@ pub mod encoded_size;
 pub use encoded_size::ConstEncodedSize;
 pub use encoded_size::EncodedSize;
 pub use encoded_size::SelfEncodedSize;
+
+pub mod parseable;
+pub use parseable::Parseable;
+
+pub mod serializable;
+pub use serializable::Serializable;
